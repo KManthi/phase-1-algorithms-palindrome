0d6ba80428ffadd93723bde8ca50253364fdc2f5
Daniel <dani3lkamanthi@gmail.com>
2024-04-05 16:47:48 +0300

    Modifies index.js, package-lock.json

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+ let reversedWord = reverse(word);
+ return word === reversedWord;
 }
 
+function reverse(word) {
+  return word.split('').reverse().join('');
+}
 /* 
-  Add your pseudocode here
+  If the string is the same forwards and backwords it should return true:
+  -Reverse the input string.
+  -Check wether the reverse input is equal to the initial input
+  -If it is, return true if not return false
 */
 
 /*
-  Add written explanation of your solution here
+  A function called reverse takes in a word and splits the word into an array, reverses the order and then joins them back up into a string.
+  The function isPalindrome takes in a word and assigns a variable reverseWord to the reverse f the word. 
+  It then checks whether the reverse and the initial are equal.
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +29,18 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome(""));
+
+
 }
 
 module.exports = isPalindrome;
